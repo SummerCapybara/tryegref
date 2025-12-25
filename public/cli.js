137df70eec86f0648cd61ca560faf45157b1d517
SummerCapybara <prpl40712@gmail.com>
2025-12-25 19:41:41 +0300

    added an option to send a message by pressing enter

--- public/cli.js
+++ public/cli.js
@@ -4,17 +4,35 @@ ws.onopen = () => ws.send("hi");
 var ulist = document.querySelector(".uli");
 var input = document.querySelector("input");
 var btn = document.querySelector(".btn");
+var isFocused = false;
+window.addEventListener("focusin", (event) => {
+  if (event.target == input) {
+    console.log("focus");
+    isFocused = true;
+  }
+});
+window.addEventListener("focusout", (event) => {
+  if (event.target == input) {
+    console.log("blur");
+    isFocused = false;
+  }
+});
 btn.onclick = () => {
   let msgcontent = input.value;
   if (msgcontent.trim()) {
     if (msgcontent.length > 1000) {
-      alert("Length of a message can't be more than 10!");
+      alert("Length of a message can't be more than 1000!");
       return;
     }
     ws.send(msgcontent);
     input.value = "";
   }
 };
+window.addEventListener("keydown", (event) => {
+  if (event.key === "Enter" && isFocused) {
+    btn.click();
+  }
+});
 ws.onmessage = (e) => {
   console.log(e.data);
   let li = document.createElement("li");
--- src/cli.ts
+++ src/cli.ts
@@ -5,14 +5,37 @@ const ulist = document.querySelector(".uli") as HTMLUListElement;
 const input = document.querySelector("input") as HTMLInputElement;
 const btn = document.querySelector(".btn") as HTMLButtonElement;
 
+let isFocused = false;
+
+window.addEventListener("focusin", (event: FocusEvent) => {
+    if (event.target == input) {
+        console.log("focus")
+        isFocused = true;
+    }
+})
+window.addEventListener("focusout", (event: FocusEvent) => {
+    if (event.target == input) {
+        console.log("blur")
+        isFocused = false;
+    }
+
+})
+
+
 btn.onclick = () => {
     let msgcontent = input.value;
     if (msgcontent.trim()) {
-        if (msgcontent.length > 1000) {alert("Length of a message can't be more than 10!"); return}; 
+        if (msgcontent.length > 1000) {alert("Length of a message can't be more than 1000!"); return}; 
         ws.send(msgcontent);
         input.value = "";
     }
 }
+window.addEventListener("keydown", (event: KeyboardEvent) => {
+    if (event.key === "Enter" && isFocused) {
+        btn.click();
+    }
+});
+
 ws.onmessage = (e) => {
     console.log(e.data);
     let li = document.createElement("li");
